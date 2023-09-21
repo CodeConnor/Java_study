ba2d7caed74989776773f4df8bd32d3f194a7656
Connor <xiaobang199806@163.com>
2023-09-21 13:50:36 +0800

    practice

---
+++ day01-code/src/ConstantDemo.java
@@ -0,0 +1,10 @@
+public class ConstantDemo {
+    public static void main(String[] args) {
+        System.out.println(18);
+        System.out.println(-1000.11);
+        System.out.println("HelloHello");
+        System.out.println('a');
+        System.out.println(true);
+        System.out.println(1 > 2);
+    }
+}
--- day01-code/src/HelloJava.java
+++ day01-code/src/HelloJava.java
@@ -1,5 +1,11 @@
+// 通过class 定义类
 public class HelloJava {
+    /*
+        下面是main方法, 以及程序的入口
+     */
     public static void main(String[] args) {
+        System.out.println("Hello Java"); // 输出语句
+        System.out.println("Hello Java");
         System.out.println("Hello Java");
     }
 }
