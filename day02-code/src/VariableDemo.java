3516e21fac2d80876c52e05831e2ef8d596ffc8f
Connor <xiaobang199806@163.com>
2023-09-21 14:31:42 +0800

    day02 practice

---
+++ day02-code/src/VariableDemo.java
@@ -0,0 +1,11 @@
+public class VariableDemo {
+    public static void main(String[] args){
+        // 定义变量
+        int age = 18;
+        // 使用变量
+        System.out.println(age);
+
+        age = 29;
+        System.out.println(age);
+    }
+}
