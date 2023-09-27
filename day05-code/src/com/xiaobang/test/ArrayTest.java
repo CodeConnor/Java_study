08a2e5c13f93715449c70fd8daecb2fe8998fe18
Connor <xiaobang199806@163.com>
2023-09-27 10:52:26 +0800

    update test

--- day05-code/src/com/xiaobang/test/ArrayTest.java
+++ day05-code/src/com/xiaobang/test/ArrayTest.java
@@ -2,20 +2,29 @@ package com.xiaobang.test;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        int sum = evenSum();
-        System.out.println("偶数和:" + sum);
+        int evenSum = evenSum();
+        System.out.println("偶数和:" + evenSum);
 
         int[] arr1 = {3, 44, 33, 55, 22};
         int max = getMax(arr1);
         System.out.println("最大值:" + max);
 
-        int[] arr2 = {100,50,20,90,90};
-        int[] result = getGrade(arr2);
-        System.out.println("最大值:" + result[0]);
-        System.out.println("最小值:" + result[1]);
-        System.out.println("总成绩:" + result[2]);
-        System.out.println("平均成绩:" + result[3]);
-        System.out.println("低于平均值的个数:" + result[4]);
+        int[] grade = {100, 50, 20, 90, 90};
+        int max1 = getMax(grade);
+        System.out.println("最高分:" + max1);
+
+        int min = getMin(grade);
+        System.out.println("最低分:" + min);
+
+        int sum = getSum(grade);
+        System.out.println("总成绩:" + sum);
+
+        double  avg = getAvg(grade);
+        System.out.println("平均值:" + avg);
+
+        int count = getCount(grade);
+        System.out.println("低于平均值的个数:" + count);
+
 
     }
 
@@ -23,7 +32,7 @@ public class ArrayTest {
         需求 : 已知数组元素为{11,22,33,44,55}
         请将数组中偶数元素取出并求和,最后打印求和结果
      */
-    public static int evenSum(){
+    public static int evenSum() {
         // 定义数组
         int[] arr1 = {11, 22, 33, 44, 55};
         // 定义变量存储偶数和
@@ -31,7 +40,7 @@ public class ArrayTest {
         // 遍历数组
         for (int i = 0; i < arr1.length; i++) {
             // 判断偶数并求和
-            if (arr1[i] % 2 == 0){
+            if (arr1[i] % 2 == 0) {
                 sum += arr1[i];
             }
         }
@@ -43,7 +52,7 @@ public class ArrayTest {
         需求 :
         请找出任意整数数组中最大值并打印在控制台
      */
-    public static int getMax(int[] arr){
+    public static int getMax(int[] arr) {
         // 将数组第一个元素赋值给max, 假设第一个元素是最大值
         int max = arr[0];
         // 从第二个元素开始依次与max比较
@@ -60,13 +69,14 @@ public class ArrayTest {
     /*
         需求:
             已知班级学生成绩为int[] arr = {100,50,20,90,90};
+            可分别用方法逐个实现该功能
             1.找出数组最大值,并打印在控制台
             2.找出数组最小值,并打印在控制台
             3.求总成绩,并打印在控制台
             4.计算出平均值,并打印在控制台
             5.统计出低于平均值元素的个数
      */
-    public static int[] getGrade(int[] arr){
+    public static int[] getGrade(int[] arr) {
         // 定义变量存放成绩
         int max = arr[0];
         int min = arr[0];
@@ -81,8 +91,8 @@ public class ArrayTest {
             }
 
             // 求最小值
-            if (min > arr[i]) {
-                min = arr[i];
+            if ( min > arr[i]) {
+                 min = arr[i];
             }
 
             // 求平均值
@@ -103,5 +113,53 @@ public class ArrayTest {
         int[] result = {max, min, sum, avg, count};
         return result;
 
+    }
+
+    /**
+     * 求数组最小值
+     */
+    public static int getMin(int[] arr){
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    /**
+     * 求数组和
+     */
+    public static int getSum(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    /**
+     * 求数组元素平均值
+     */
+    public static double getAvg(int[] arr){
+        int sum = getSum(arr);
+
+        double avg = sum / arr.length;
+        return avg;
+    }
+
+    /**
+     * 统计数组中元素低于平均值的个数
+     */
+    public static int getCount(int[] arr){
+        double avg = getAvg(arr);
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < avg) {
+                count++;
+            }
+        }
+        return count;
     }
 }
